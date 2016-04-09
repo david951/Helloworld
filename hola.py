9d77396f224b359577c21c93795b92ea36f964ed
david951 <davidsantiagosuarez@gmail.com>
2016-04-09 13:06:58 -0500

    Added files via upload

---
+++ hola.py
@@ -0,0 +1,27 @@
+
+var=  int (raw_input("ingrese el primer numero"))
+var2=  int (raw_input("ingres el segundo numero"))
+suma= var+var2
+print suma
+print"ingrese para la resta"
+
+resta= var-var2
+print resta
+print "ingrese para la multiplicacion"
+
+multiplicacion=var*var2
+print multiplicacion
+print"ingrese para la division"
+
+division=var/var2
+if var2!=0:
+    print division
+print "ingrese para el residuo"
+
+porcentaje=var%var2
+print porcentaje 
+
+    
+    
+
+                    
