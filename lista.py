ff54b61e09e5f1d0b03cd067352b5bde700af66f
david951 <davidsantiagosuarez@gmail.com>
2016-04-16 12:42:33 -0500

    Added files via upload

---
+++ lista.py
@@ -0,0 +1,24 @@
+lista1= [ ]
+a=0
+b=0
+mayor=0
+n=int(input("digite cuantos numeros desea"))
+while n>a:
+    num=int(input("ingrese el numero"))
+    lista1.append(num)
+    a=a+1
+
+print lista1
+for i in range (n):
+    lista1[i]
+    if mayor<num:
+        mayor=num
+print mayor
+n=n+1
+
+
+    
+    
+
+
+
